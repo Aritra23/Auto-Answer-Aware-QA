316838207dc914646e4d8dcb3841e51d4f633ae7
Aritra Kumar Lahiri <lahiri.aritra@gmail.com>
2023-05-11 00:20:25 -0400

    Fix model output issue and add pipeline tasks

--- app.py
+++ app.py
@@ -3,36 +3,93 @@ import json
 import smtplib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
+from itertools import groupby
+
 # from email.mime.base import MIMEBase
 # from email.mime.text import MIMEText
 # from email.utils import COMMASPACE
 # from email import encoders
 from flask import Flask, render_template, request, send_file, url_for, redirect
-from pipelines import pipeline # Replace with your own function for generating answers
+# from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
+from pipelines import pipeline  # Replace with your own function for generating answers
 from question_generation import clean_text
+# from huggingface_hub import login
+# access_token = "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"
+# login(token = access_token)
+from transformers import AutoModel, AutoModelForSeq2SeqLM, AutoTokenizer
+
+access_token = "hf_..."
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/answer", methods=["POST"])
 def answer():
     passage = request.form["passage"]
-    cleaned_passage = clean_text(passage)
+    # cleaned_passage = clean_text(passage)
     # question = request.form["question"]
-    nlp = pipeline("question-generation", model="valhalla/t5-small-qg-prepend", qg_format="prepend")
-    # nlp = pipeline("question-generation")
+    # model = AutoModel.from_pretrained("valhalla/t5-small-qg-prepend", use_auth_token='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')
+    # nlp = pipeline('question-generation', model='valhalla/t5-small-qg-prepend', qg_format="prepend", use_auth_token='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')
+    # nlp = pipeline("question-answering", model = "distilbert-base-cased-distilled-squad", tokenizer = "bert-base-cased")
+    # model = AutoModelForSeq2SeqLM.from_pretrained("valhalla/t5-small-e2e-qg", use_auth_token='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')
+    # tokenizer = AutoTokenizer.from_pretrained("valhalla/t5-small-e2e-qg")
+    # nlp = pipeline("e2e-qg", use_auth_token='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')
+    # nlp = pipeline("multitask-qa-qg", use_auth_token='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')
+
+    # tokenizer = AutoTokenizer.from_pretrained("valhalla/t5-small-qa-qg-hl", use_auth_token='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')
+    #
+    # model = AutoModelForSeq2SeqLM.from_pretrained("valhalla/t5-small-qa-qg-hl", use_auth_token='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')
+    # tokenizer = AutoTokenizer.from_pretrained("valhalla/t5-small-qg-hl", use_auth_token='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')
+
+    # model = AutoModelForSeq2SeqLM.from_pretrained("valhalla/t5-small-qg-prepend", use_auth_token='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')
+
+    # nlp = pipeline("question-generation", use_auth_token='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')
+    nlp = pipeline("question-generation", model="valhalla/t5-small-qg-prepend", qg_format="prepend", use_auth_token='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')
     # nlp = pipeline("multitask-qa-qg", model="valhalla/distilt5-qa-qg-hl-6-4")
-    answer = nlp(cleaned_passage)
+    # qg_model = AutoModelForSeq2SeqLM.from_pretrained("ramsrigouthamg/t5_squad_v1", use_auth_token='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')
+    # qg_tokenizer = AutoTokenizer.from_pretrained("ramsrigouthamg/t5_squad_v1", use_auth_token='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')
+    # qa_model = pipeline("question-answering", model="mrm8488/t5-small-finetuned-squad-qgen",
+    #                     tokenizer="t5-base", use_auth_token='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')
+    # print(passage)
+    answer = nlp(passage)
+
+    answer = [{'question': key, 'answer': max(item['answer'] for item in values)}
+                     for key, values in groupby(answer, lambda dct: dct['question'])]
+
     for x in answer:
         if "<pad> " in x['answer']:
             x['answer'] = x['answer'].replace("<pad> ", "")
+    selected_task = request.form["task"]
+    question_list = []
+    # Use the selected model
+    if selected_task == "single-qa":
+
+        for x in answer:
+            answer_task1 = x
+            answer = answer_task1
+            break
+
+    if selected_task == "e2e-qg":
+        nlp = pipeline("e2e-qg", use_auth_token='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')
+        answer = nlp(passage)
+        # for x in answer:
+        #     question_list.append(x['question'])
+        #     answer = question_list
+    if selected_task == "e2e-qa":
+        answer = answer
+
     with open('answer.json', 'w') as outfile:
         json.dump(answer, outfile)
+        # json.dumps(qa_pairs, indent=4)
     # return render_template('answer.html', passage=cleaned_passage, question=question, answer=answer)
-    return render_template("answer.html", passage=passage, answer=answer)
+    return render_template("answer.html", answer=answer)
+
 
 @app.route('/download')
 def download():
@@ -42,13 +99,13 @@ def download():
 @app.route('/email', methods=['POST'])
 def email_answer():
     email = request.form['email']
-    passage = request.form['passage']
+    # passage = request.form['passage']
     # question = request.form['question']
     answer = request.form['answer']
 
     # Create a JSON file for the answer data
     answer_data = {
-        "passage": passage,
+        # "passage": passage,
         # "question": question,
         "answer": answer
     }
--- question_generation.py
+++ question_generation.py
@@ -6,13 +6,13 @@ def clean_text(text):
     '''Make text lowercase, remove text in square brackets,remove links,remove punctuation
     and remove words containing numbers.'''
    # df["Toke = df["Tokenized_description"].astype(str)
-    text = text.lower()
-    text = re.sub('\[.*?\]', '', text)
+   #  text = text.lower()
+   #  text = re.sub('\[.*?\]', '', text)
     text = re.sub('https?://\S+|www\.\S+', '', text)
     text = re.sub('<.*?>+', '', text)
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    # text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\n', '', text)
-    text = re.sub('\w*\d\w*', '', text)
+    # text = re.sub('\w*\d\w*', '', text)
     return text
 
 
